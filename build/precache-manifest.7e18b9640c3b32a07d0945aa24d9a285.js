35ba37fa7a6d3491c30fd7b1aaa11aa28878c568
Den <denisflip@gmail.com>
2020-02-09 23:01:00 +0300

    build

---
+++ build/precache-manifest.7e18b9640c3b32a07d0945aa24d9a285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7da2ee2236e6b07d52a3afff8676346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3039f888818f06d5af56",
+    "url": "/static/css/main.a60c6cf7.chunk.css"
+  },
+  {
+    "revision": "40ca14c6248a049ec1e9",
+    "url": "/static/js/2.b0fa00dd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b0fa00dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3039f888818f06d5af56",
+    "url": "/static/js/main.8ca59602.chunk.js"
+  },
+  {
+    "revision": "2f9cea63869bec370274",
+    "url": "/static/js/runtime-main.fb559340.js"
+  }
+]);
